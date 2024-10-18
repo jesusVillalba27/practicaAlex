ca4b255b3e8a30c4d87ecae48d172bf00920b5f1
jesusVillalba27 <jesus.villalbav@cecar.edu.co>
2024-10-17 22:05:19 -0500

    proyecto

---
+++ alex/alex/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace alex
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ alex/alex/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace alex
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
