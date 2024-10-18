34c0cd498102e7692ec0055be935a54b20824fad
Jose-Pater294 <jose.paterninas@cecar.edu.co>
2024-10-17 22:57:48 -0500

    agregue el Page2

--- alex/alex/MainPage.xaml.cs
+++ alex/alex/MainPage.xaml.cs
@@ -8,18 +8,12 @@
         {
             InitializeComponent();
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void OnPage2ButtonClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            await Navigation.PushAsync(new Page2());
         }
+
+
     }
 
 }
---
+++ alex/alex/Page2.xaml.cs
@@ -0,0 +1,9 @@
+namespace alex;
+
+public partial class Page2 : ContentPage
+{
+	public Page2()
+	{
+		InitializeComponent();
+	}
+}
